1cd7ee61a7a3d5bf3a4c00eaf1f0a1804df1d8f0
Alejandro Pérez Durán <apduran95@gmail.com>
2025-11-12 01:12:37 -0500

    refactor: remove unused grid components and simplify gallery structure

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-import { GridTileImage } from "components/grid/tile";
 import { Gallery } from "components/product/gallery";
 import { ProductDescription } from "components/product/product-description";
 import { HIDDEN_PRODUCT_TAG } from "lib/constants";
 import { getProduct, getProductRecommendations } from "lib/data";
 import { Image } from "lib/types";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export async function generateMetadata(props: {
@@ -122,25 +120,7 @@ async function RelatedProducts({ id }: { id: string }) {
           <li
             key={product.handle}
             className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
-          >
-            <Link
-              className="relative h-full w-full"
-              href={`/product/${product.handle}`}
-              prefetch={true}
-            >
-              <GridTileImage
-                alt={product.title}
-                label={{
-                  title: product.title,
-                  amount: product.priceRange.maxVariantPrice.amount,
-                  currencyCode: product.priceRange.maxVariantPrice.currencyCode,
-                }}
-                src={product.featuredImage?.url}
-                fill
-                sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
-              />
-            </Link>
-          </li>
+          ></li>
         ))}
       </ul>
     </div>
